7b3faea83fa93b59dab90eadad046d77021d453f
3ll3n <ellengrafton01@gmail.com>
2017-03-18 18:10:22 +0000

    Created tests and methods for Item getter and setters - name and price

--- app/src/test/java/com/codeclan/example/shoppingbasket/ItemTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/ItemTest.java
@@ -1,6 +1,9 @@
 package com.codeclan.example.shoppingbasket;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by user on 18/03/2017.
@@ -17,5 +20,27 @@ public class ItemTest {
         bread = new Bread("Wholemeal", 2);
         milk = new Milk("Skimmed", 1);
         butter = new Butter("Unsalted", 2);
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("Wholemeal", bread.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        milk.setName("Semi skimmed");
+        assertEquals("Semi skimmed", milk.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        assertEquals(2, butter.getPrice());
+    }
+
+    @Test
+    public void testSetPrice() {
+        butter.setPrice(3);
+        assertEquals(3, butter.getPrice());
     }
 }
