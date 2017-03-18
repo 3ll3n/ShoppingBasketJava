de7ed6f9ca59d2a4efb3b29617b0cadd349621ff
3ll3n <ellengrafton01@gmail.com>
2017-03-18 18:40:53 +0000

    Updated ShoppingBasket/Test

--- app/src/main/java/com/codeclan/example/shoppingbasket/ShoppingBasket.java
+++ app/src/main/java/com/codeclan/example/shoppingbasket/ShoppingBasket.java
@@ -1,8 +1,17 @@
 package com.codeclan.example.shoppingbasket;
 
+import java.util.ArrayList;
+
 /**
  * Created by user on 18/03/2017.
  */
 
 public class ShoppingBasket {
+
+    private ArrayList<Item> items;
+
+    public ShoppingBasket() {
+        this.items = new ArrayList<Item>();
+    }
+    
 }
--- app/src/test/java/com/codeclan/example/shoppingbasket/ShoppingBasketTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/ShoppingBasketTest.java
@@ -1,8 +1,27 @@
 package com.codeclan.example.shoppingbasket;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by user on 18/03/2017.
  */
 
 public class ShoppingBasketTest {
+
+    ShoppingBasket shoppingbasket;
+
+    Bread bread;
+    Milk milk;
+
+    @Before
+    public void before() {
+        shoppingbasket = new ShoppingBasket();
+        bread = new Bread("Baguette", 1);
+        milk = new Milk("full fat", 2);
+    }
+
+
 }
