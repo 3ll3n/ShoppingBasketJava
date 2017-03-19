049344d6e31c9a2387cd4238f2aaca69b58a919b
3ll3n <ellengrafton01@gmail.com>
2017-03-19 19:51:48 +0000

    Added IDiscount interface and discount classes

---
+++ app/src/main/java/com/codeclan/example/shoppingbasket/TenPercentOffTwentyPoundSpend.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.shoppingbasket;
+
+/**
+ * Created by user on 19/03/2017.
+ */
+
+public class TenPercentOffTwentyPoundSpend implements IDiscount {
+
+//    public void applyDiscount() {
+//
+//    }
+}
---
+++ app/src/test/java/com/codeclan/example/shoppingbasket/BoGoFTest.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.shoppingbasket;
+
+/**
+ * Created by user on 19/03/2017.
+ */
+
+public class BoGoFTest {
+}
